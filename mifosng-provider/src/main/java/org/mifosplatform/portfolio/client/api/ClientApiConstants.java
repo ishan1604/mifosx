4a30cfeb3993ff9c622146b6c88902f5c61b15e5
CieyouRaoul <cieyouraoul@musoni.eu>
2014-04-29 12:08:03 +0200

    abillity to modify client submission date

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/api/ClientApiConstants.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/api/ClientApiConstants.java
@@ -68,7 +68,7 @@ public class ClientApiConstants {
 
     public static final Set<String> CLIENT_UPDATE_REQUEST_DATA_PARAMETERS = new HashSet<String>(Arrays.asList(localeParamName,
             dateFormatParamName, accountNoParamName, externalIdParamName, mobileNoParamName, firstnameParamName, middlenameParamName,
-            lastnameParamName, fullnameParamName, activeParamName, activationDateParamName, staffIdParamName, savingsProductIdParamName));
+            lastnameParamName, fullnameParamName, activeParamName, activationDateParamName, staffIdParamName, savingsProductIdParamName,submittedOnDateParamName));
 
     /**
      * These parameters will match the class level parameters of
