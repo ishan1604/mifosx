6aa0dd88cd0320555989d06b970ccbb4ec1e1ea2
andrew-dzak <andrewdzakpasu@musoni.eu>
2014-05-02 09:56:33 +0200

    mlite-257 done

--- mifosng-provider/src/main/java/org/mifosplatform/commands/service/SynchronousCommandProcessingService.java
+++ mifosng-provider/src/main/java/org/mifosplatform/commands/service/SynchronousCommandProcessingService.java
@@ -514,9 +514,11 @@ public class SynchronousCommandProcessingService implements CommandProcessingSer
                 handler = this.applicationContext.getBean("assignGroupStaffCommandHandler", NewCommandSourceHandler.class);
             } else if (wrapper.isTransferClientsBetweenGroups()) {
                 handler = this.applicationContext.getBean("transferClientsBetweenGroupsCommandHandler", NewCommandSourceHandler.class);
-            } else if (wrapper.isGroupClose()) {
+            }else if (wrapper.isGroupClose()) {
                 handler = this.applicationContext.getBean("closeGroupCommandHandler", NewCommandSourceHandler.class);
-            } else {
+            }else if (wrapper.isTransferGroupBetweenBranches()) {
+                handler = this.applicationContext.getBean("transferGroupBetweenBranchesCommandHandler", NewCommandSourceHandler.class);
+            }else {
                 throw new UnsupportedCommandException(wrapper.commandName());
             }
         } else if (wrapper.isCenterResource()) {
