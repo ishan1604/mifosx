0398d1f655f626496bdd8e25772961c1e1fc7dd6
andrew-dzak <andrewdzakpasu@musoni.eu>
2014-05-02 09:56:33 +0200

    mlite-257 done

--- mifosng-provider/src/main/java/org/mifosplatform/commands/service/CommandWrapperBuilder.java
+++ mifosng-provider/src/main/java/org/mifosplatform/commands/service/CommandWrapperBuilder.java
@@ -388,6 +388,7 @@ public class CommandWrapperBuilder {
         return this;
     }
 
+
     public CommandWrapperBuilder proposeAndAcceptClientTransfer(final Long clientId) {
         this.actionName = "PROPOSEANDACCEPTTRANSFER";
         this.entityName = "CLIENT";
@@ -1216,6 +1217,14 @@ public class CommandWrapperBuilder {
         this.entityId = sourceGroupId;
         this.groupId = sourceGroupId;
         this.href = "/groups/" + sourceGroupId + "?command=transferClients";
+        return this;
+    }
+    public CommandWrapperBuilder transferGroupBetweenBranches(final Long sourceGroupId) {
+        this.actionName = "TRANSFERGROUP";
+        this.entityName = "GROUP";
+        this.entityId = sourceGroupId;
+        this.groupId = sourceGroupId;
+        this.href = "/groups/" + sourceGroupId + "?command=transferGroup";
         return this;
     }
 
