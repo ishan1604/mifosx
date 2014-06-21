0331ad5f72d7f9f76aac4dc243c0d270bc36a441
emmanuel <emmanuelnnaa@musoni.eu>
2014-06-21 12:44:09 +0200

    A couple of lines was missing in the new release, added those lines from '16896e2fcc320d8b436593f7e4489f1b9903cb32' commit

--- mifosng-provider/src/main/java/org/mifosplatform/commands/api/MakercheckersApiResource.java
+++ mifosng-provider/src/main/java/org/mifosplatform/commands/api/MakercheckersApiResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -117,6 +118,16 @@ public class MakercheckersApiResource {
             throw new UnrecognizedQueryParamException("command", commandParam);
         }
         return this.toApiJsonSerializerAudit.serialize(result);
+    }
+    
+    @PUT
+    @Path("{auditId}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public String rejectMakerCheckerEntry(@PathParam("auditId") final Long auditId) {
+        final Long id = this.writePlatformService.rejectEntry(auditId);
+
+        return this.toApiJsonSerializerAudit.serialize(CommandProcessingResult.commandOnlyResult(id));
     }
 
     private boolean is(final String commandParam, final String commandValue) {
