67739025da63afaefc96ea4479afd027f8912ee9
emmanuel <emmanuelnnaa@musoni.eu>
2014-06-24 12:24:53 +0200

    sql file for the updated sms module

---
+++ mifosng-db/migrations/core_db/V167_2_add_columns_to_sms_message_outbound_table.sql
@@ -0,0 +1,16 @@
+ALTER TABLE `sms_messages_outbound` ADD `external_id` BIGINT(20) NULL COMMENT 'ID of the message in the smsOutboundMessage table in the mlite-sms database' AFTER `id`;
+ALTER TABLE `sms_messages_outbound` ADD `source_address` VARCHAR(50) NULL AFTER `status_enum` COMMENT 'Sender of the SMS message.';
+ALTER TABLE `sms_messages_outbound` CHANGE `message` `message` VARCHAR(254) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+
+create table sms_configuration ( id int primary key auto_increment, name varchar(50) not null, value varchar(200) not null );
+
+INSERT INTO `sms_configuration` (`id`, `name`, `value`) VALUES (NULL, 'API_BASE_URL', 'http://localhost:8080/mlite-sms/api/v1/sms');
+
+INSERT INTO `sms_configuration` (`id`, `name`, `value`) VALUES (NULL, 'API_AUTH_USERNAME', 'root');
+
+INSERT INTO `sms_configuration` (`id`, `name`, `value`) VALUES (NULL, 'API_AUTH_PASSWORD', 'localhost');
+
+INSERT INTO `sms_configuration` (`id`, `name`, `value`) VALUES (NULL, 'OUTBOUND_SMS_LIMIT', '1000');
+
+INSERT INTO `sms_configuration` (`id`, `name`, `value`) VALUES (NULL, 'SMS_SOURCE_ADDRESS', 'Musoni Services');
