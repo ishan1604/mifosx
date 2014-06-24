86e781d3c2909876c4c5377430a51bccb3713295
CieyouRaoul <cieyouraoul@musoni.eu>
2014-06-24 11:12:20 +0200

    allow null value for loanProduct default princial value

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/loanproduct/serialization/LoanProductDataValidator.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/loanproduct/serialization/LoanProductDataValidator.java
@@ -109,7 +109,7 @@ public final class LoanProductDataValidator {
         baseDataValidator.reset().parameter("inMultiplesOf").value(inMultiplesOf).ignoreIfNull().integerZeroOrGreater();
 
         final BigDecimal principal = this.fromApiJsonHelper.extractBigDecimalWithLocaleNamed("principal", element);
-        baseDataValidator.reset().parameter("principal").value(principal).notNull().positiveAmount();
+        baseDataValidator.reset().parameter("principal").value(principal).positiveAmount();
 
         final String minPrincipalParameterName = "minPrincipal";
         BigDecimal minPrincipalAmount = null;
@@ -125,17 +125,18 @@ public final class LoanProductDataValidator {
             baseDataValidator.reset().parameter(maxPrincipalParameterName).value(maxPrincipalAmount).ignoreIfNull().positiveAmount();
         }
 
-        if (maxPrincipalAmount != null && maxPrincipalAmount.compareTo(BigDecimal.ZERO) != -1) {
+        if (maxPrincipalAmount != null && maxPrincipalAmount.compareTo(BigDecimal.ZERO) != -1 ) {
+
             if (minPrincipalAmount != null && minPrincipalAmount.compareTo(BigDecimal.ZERO) != -1) {
                 baseDataValidator.reset().parameter(maxPrincipalParameterName).value(maxPrincipalAmount).notLessThanMin(minPrincipalAmount);
-                if (minPrincipalAmount.compareTo(maxPrincipalAmount) <= 0) {
+                if (minPrincipalAmount.compareTo(maxPrincipalAmount) <= 0 && principal != null ) {
                     baseDataValidator.reset().parameter("principal").value(principal)
                             .inMinAndMaxAmountRange(minPrincipalAmount, maxPrincipalAmount);
                 }
-            } else {
+            } else if(principal != null) {
                 baseDataValidator.reset().parameter("principal").value(principal).notGreaterThanMax(maxPrincipalAmount);
             }
-        } else if (minPrincipalAmount != null && minPrincipalAmount.compareTo(BigDecimal.ZERO) != -1) {
+        } else if (minPrincipalAmount != null && minPrincipalAmount.compareTo(BigDecimal.ZERO) != -1 && principal !=null) {
             baseDataValidator.reset().parameter("principal").value(principal).notLessThanMin(minPrincipalAmount);
         }
 
@@ -440,7 +441,7 @@ public final class LoanProductDataValidator {
 
         if (this.fromApiJsonHelper.parameterExists("principal", element)) {
             final BigDecimal principal = this.fromApiJsonHelper.extractBigDecimalWithLocaleNamed("principal", element);
-            baseDataValidator.reset().parameter("principal").value(principal).notNull().positiveAmount();
+            baseDataValidator.reset().parameter("principal").value(principal).positiveAmount();
         }
 
         if (this.fromApiJsonHelper.parameterExists("inArrearsTolerance", element)) {
