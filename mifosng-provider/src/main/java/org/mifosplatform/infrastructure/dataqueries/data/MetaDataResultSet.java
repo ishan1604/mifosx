2b14508289f3dab823f2a3d12f1db577083ec98b
Emmanuel Nnaa <emmanuelnnaa@musoni.eu>
2014-10-15 17:07:22 +0200

    commit for MIFOSX-1597 (added meta data to dataTable) - Authored by Andrew, cherry-picked from v1.26_Release (dffa2ca19a8d41c070dc03ede0495e320df915cc)

---
+++ mifosng-provider/src/main/java/org/mifosplatform/infrastructure/dataqueries/data/MetaDataResultSet.java
@@ -0,0 +1,45 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.mifosplatform.infrastructure.dataqueries.data;
+
+public class MetaDataResultSet {
+
+    private Long id;
+    private String columnName;
+    private String labelName;
+    private Long order;
+    private boolean systemDefined;
+
+
+    public static MetaDataResultSet createMetaDataResultSet(final Long id,final String columnName,final String labelName, final Long order,final boolean systemDefined){
+        return new MetaDataResultSet(id,columnName,labelName,order,systemDefined);
+    }
+    private MetaDataResultSet(final Long id,final String columnName, final String labelName, final Long order,final boolean systemDefined) {
+        this.id = id;
+        this.columnName = columnName;
+        this.labelName = labelName;
+        this.order = order;
+        this.systemDefined = systemDefined;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public String getLabelName() {
+        return labelName;
+    }
+
+    public Long getOrder() {
+        return order;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public boolean isSystemDefined() {return this.systemDefined;}
+}
