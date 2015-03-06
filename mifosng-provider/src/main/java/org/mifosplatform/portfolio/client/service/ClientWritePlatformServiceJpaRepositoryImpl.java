23f5ad73678cab94f7eb8c12fa3d1c2efa54f298
CieyouRaoul <cieyouraoul@musoni.eu>
2015-03-06 12:48:38 +0100

    fixed issue when closing client with, it throwed a null pointer exception

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/service/ClientWritePlatformServiceJpaRepositoryImpl.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/service/ClientWritePlatformServiceJpaRepositoryImpl.java
@@ -508,7 +508,11 @@ public class ClientWritePlatformServiceJpaRepositoryImpl implements ClientWriteP
                 throw new InvalidClientStateTransitionException("close", "is.under.transfer", errorMessage);
             }
 
-            if (client.isNotPending() && client.getActivationLocalDate().isAfter(closureDate)) {
+
+            //logger.error("is closure date:"+client.getActivationLocalDate().isAfter(closureDate));
+
+
+            if (client.isNotPending() && client.getActivationLocalDate()!=null && client.getActivationLocalDate().isAfter(closureDate)) {
                 final String errorMessage = "The client closureDate cannot be before the client ActivationDate.";
                 throw new InvalidClientStateTransitionException("close", "date.cannot.before.client.actvation.date", errorMessage,
                         closureDate, client.getActivationLocalDate());
