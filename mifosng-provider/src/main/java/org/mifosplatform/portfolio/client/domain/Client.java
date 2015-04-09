b17f54e48a5d4d7491d997ca79dbf725b75043ef
raoulngakam <raoulngakam@gmail.com>
2015-04-09 12:49:26 +0200

    commit to set closure properties of reactivated closed client entity to null - Authored by Raoul, cherry-picked from v1.26_Release (7b497d3bf8269aa5cbfe04ac543dac6b6fc03802)
    MifosX jira item - https://issues.apache.org/jira/browse/FINERACT-57

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/domain/Client.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/client/domain/Client.java
@@ -403,6 +403,11 @@ public final class Client extends AbstractPersistable<Long> {
         this.officeJoiningDate = this.activationDate;
         this.status = ClientStatus.ACTIVE.getValue();
 
+        // in case a closed client is being re open
+        this.closureDate = null;
+        this.closureReason = null;
+        this.closedBy = null;
+
         validate();
     }
 
