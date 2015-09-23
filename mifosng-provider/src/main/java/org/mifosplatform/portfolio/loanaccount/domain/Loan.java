a950c79eedae1cfcf261388f1a1ccc3d2f990177
andrew-dzak <andrewdzakpasu@musoni.eu>
2015-09-23 15:58:10 +0200

    mifosx-2271 fix waive charge JE bookings

--- mifosng-provider/src/main/java/org/mifosplatform/portfolio/loanaccount/domain/Loan.java
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/loanaccount/domain/Loan.java
@@ -971,7 +971,10 @@ public class Loan extends AbstractPersistable<Long> {
         Money unrecognizedIncome = amountWaived.zero();
         Money chargeComponent = amountWaived;
         if (isPeriodicAccrualAccountingEnabledOnLoanProduct()) {
-            Money receivableCharge = accruedCharge.minus(loanCharge.getAmountPaid(getCurrency()));
+            Money receivableCharge = Money.zero(getCurrency());
+            if(loanInstallmentNumber !=null){
+                receivableCharge = accruedCharge.minus(loanCharge.getInstallmentLoanCharge(loanInstallmentNumber).getAmountPaid(getCurrency()));
+            }else{ receivableCharge = accruedCharge.minus(loanCharge.getAmountPaid(getCurrency()));}
             if (receivableCharge.isLessThanZero()) {
                 receivableCharge = amountWaived.zero();
             }
@@ -987,7 +990,7 @@ public class Loan extends AbstractPersistable<Long> {
             feeChargesWaived = Money.zero(loanCurrency());
         }
 
-        LocalDate transactionDate = getDisbursementDate();
+        LocalDate transactionDate = DateUtils.getLocalDateOfTenant();
         if (loanCharge.isSpecifiedDueDate() || loanCharge.isPenaltyCharge()) {
             transactionDate = loanCharge.getDueLocalDate();
         }
