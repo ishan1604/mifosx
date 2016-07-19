fc6a748172f344078775f8b1432b8efc5466dd2f
stefan <stefangeers@musoni.eu>
2016-07-19 15:12:57 +0200

    added to datatables and added migration sql to resources

---
+++ mifosng-provider/src/main/resources/sql/migrations/core_db/V290_2__add_m_data_export_and_m_data_export_process.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `mifostenant-demo`.`m_data_export` (
+  `id` BIGINT(20) NOT NULL,
+  `base_entity` VARCHAR(20) NOT NULL,
+  `entity_id` BIGINT(20) NOT NULL,
+  `entity_status` INT(5) NOT NULL,
+  `submitted_on_date` DATE NOT NULL,
+  `account_no` VARCHAR(20) NULL,
+  `office_id` BIGINT(20) NULL DEFAULT NULL,
+  `display_name` VARCHAR(100) NULL DEFAULT NULL,
+  `mobile_no` VARCHAR(50) NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `mifostenant-demo`.`m_data_export_process` (
+  `id` BIGINT(20) NOT NULL,
+  `data_export_id` BIGINT(20) NOT NULL,
+  `process_status` INT(5) NOT NULL,
+  `file_name` VARCHAR(200) NULL,
+  `started_by_user_id` BIGINT(20) NOT NULL DEFAULT '1',
+  `started_date` DATE NOT NULL,
+  `ended_date` DATE NULL,
+  `error_message` MEDIUMTEXT NULL DEFAULT NULL,
+  `file_download_count` BIGINT(20) NOT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+INSERT INTO `m_permission`
+(`grouping`,`code`,`entity_name`,`action_name`,`can_maker_checker`) VALUES
+('dataexport','CREATE_DATAEXPORT','DATAEXPORT','CREATE',0),
+('dataexport','CREATE_DATAEXPORT','DATAEXPORT','READ',0),
+('dataexport','CREATE_DATAEXPORT','DATAEXPORT','DELETE',0),
+('dataexport','CREATE_DATAEXPORTPROCESS','DATAEXPORTPROCESS','CREATE',0);
+('dataexport','CREATE_DATAEXPORTPROCESS','DATAEXPORTPROCESS','READ',0);
+('dataexport','CREATE_DATAEXPORTPROCESS','DATAEXPORTPROCESS','DELETE',0);
